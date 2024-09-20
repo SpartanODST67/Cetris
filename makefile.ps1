29fb4e828243cc20b6f1f4ce6389d9ab044e9d7c
Hans Landgrebe <hlxbl20@gmail.com>
2024-09-20 15:05:56 -0500

    Forgot to link LinkedList.o

--- makefile.ps1
+++ makefile.ps1
@@ -1,5 +1,5 @@
 gcc pieces.c -o pieces.o -c
 gcc LinkedList.c -o LinkedList.o -c
 gcc Cetris.c -o Cetris.o -c
-gcc Cetris.o pieces.o -o Cetris.exe
+gcc Cetris.o pieces.o LinkedList.o -o Cetris.exe
 rm *.o
