0da2cabbaa0314f55fcd49947ec554eb4d18d620
Hans Landgrebe <hlxbl20@gmail.com>
2024-09-21 15:17:19 -0500

    Revert "Added score.c to makefile"
    This reverts commit e2c8d44c6ff60ad5c814992352a25c865c5e08f9.

--- makefile.ps1
+++ makefile.ps1
@@ -1,6 +1,5 @@
 gcc pieces.c -o pieces.o -c
-gcc score.c -o score.o -c
 gcc LinkedList.c -o LinkedList.o -c
 gcc Cetris.c -o Cetris.o -c
-gcc Cetris.o pieces.o LinkedList.o score.o -o Cetris.exe
-Remove-Item *.o
+gcc Cetris.o pieces.o LinkedList.o -o Cetris.exe
+rm *.o
